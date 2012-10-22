768149c50a5e044ac8c258e803b3f32bf6ace006
Jeremy Herr <jeremy.herr@gmail.com>
2012-10-22 23:59:43 +0100

    Added DataBlock2R1R constructor and jasmine spec

---
+++ data-blocks.js
@@ -0,0 +1,58 @@
+/**
+ * @class data block for holding a regular grid of points, with domain 2D real and range 1D real
+ * @param {Object} an object containing xMin, xMax, xStep, yMin, yMax, yStep
+ */
+function DataBlock2R1R(options) {
+	"use strict";
+
+	var xCtr,
+		yCtr,
+		k,
+		defaults = {
+			xMin:   0,
+			xMax:   1,
+			xStep: 10,
+			yMin:   0,
+			yMax:   1,
+			yStep: 10
+		};
+
+	this.parameters = {};
+
+	// Populate parameters with defaults keys, overwriting values with given options
+	for (k in defaults) {
+		if (defaults.hasOwnProperty(k)) {
+			this.parameters[k] = options[k];
+		}
+	}
+
+	/**
+	 * Create 2-dimensional array full of zeros. The zeros are to be replaced with 1-dimensional output later.
+	 */
+	this.generateDomain = function () {
+		this.data = [];
+
+		for (xCtr = 0; xCtr < this.parameters.xStep; xCtr++) {
+			this.data[xCtr] = [];
+			for (yCtr = 0; yCtr < this.parameters.yStep; yCtr++) {
+				this.data[xCtr][yCtr] = 0;
+			}
+		}
+	};
+
+	/**
+	 * Create data object for conversion to JSON
+	 */
+	this.toJSON = function () {
+		return ({
+			'parameters': this.parameters,
+			'data':       this.data
+		});
+	};
+
+}
+
+// If running in node.js, export constructor for use by require
+if (typeof (exports) === 'object') {
+	exports.DataBlock2R1R = DataBlock2R1R;
+}
---
+++ spec/data.spec.js
@@ -0,0 +1,33 @@
+var data = require('../prepare-data.js');
+
+describe("DataBlock2R1R constructor", function () {
+
+	var b = new data.DataBlock2R1R ({
+		xMin: -0.5,
+		xMax:  0.5,
+		xStep: 10,
+		yMin:  1.0,
+		yMax:  2.0,
+		yStep: 10
+	});
+
+	b.generateDomain();
+
+    it("initializes parameters as specified", function () {
+    	expect(b.parameters.xMin).toBe(-0.5);
+    	expect(b.parameters.xMax).toBe( 0.5);
+    	expect(b.parameters.xStep).toBe( 10);
+    	expect(b.parameters.yMin).toBe( 1.0);
+    	expect(b.parameters.yMax).toBe( 2.0);
+    	expect(b.parameters.yStep).toBe( 10);
+
+    });
+
+    it("initializes data range values to 0", function () {
+    	expect(b.data[0][0]).toBe(0);
+    	expect(b.data[9][9]).toBe(0);
+    });
+
+});
+
+
