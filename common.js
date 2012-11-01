bd9305f3b71f3c4018e3cf1e8f625b5faa6fc718
Jeremy Herr <jeremy.herr@gmail.com>
2012-11-01 22:44:47 +0000

    Improved common.extend to do deep copy of options/defaults objects
     - can't deal with arrays yet
     - also fixed a bug, it never worked properly in the first place if options was undefined or missing elements.

--- common.js
+++ common.js
@@ -4,12 +4,29 @@
  * @returns {Object} object containing the keys of defaults, values of defaults, overwritten by values of options
  */
 function extend (options, defaults) {
-	var k,
+	var key,
 	    parameters = {};
 
-	for (k in defaults) {
-		if (defaults.hasOwnProperty(k)) {
-			parameters[k] = options[k];
+	if (typeof (defaults) === 'object') {
+		for (key in defaults) {
+			if (defaults.hasOwnProperty(key)) {
+				if (typeof (defaults[key]) === 'string' || typeof (defaults[key]) === 'number' || typeof (defaults[key]) === 'boolean') {
+					if (typeof (options) !== 'undefined' && key in options) {
+						parameters[key] = options[key];
+					} else {
+						parameters[key] = defaults[key];
+					}
+				} else if (typeof (defaults[key]) === 'array') {
+					// TODO: deal with arrays!
+
+				} else if (typeof (defaults[key]) === 'object') {
+					if (typeof (options) !== 'undefined' && key in options) {
+						parameters[key] = extend(options[key], defaults[key]);
+					} else {
+						parameters[key] = extend(defaults[key], defaults[key]);
+					}
+				}
+			}
 		}
 	}
 
